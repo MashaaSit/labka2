24cd4c347b8356b03b1217c2bd6fa8866bdf3aa5
Maria Sitnikova <mariyadance2004@gmail.com>
2024-03-14 09:52:45 +0300

    First commit

---
+++ classes.cpp
@@ -0,0 +1,30 @@
+//#include "header.h"
+//
+//class Student
+//{
+//public:
+//	Student(string n, int g);
+//	~Student();
+//
+//	void print();
+//
+//private:
+//	string name;
+//	int grade; // 2 - плохо 3 - 55% правильно 4 - хорошо
+//};
+//
+//Student::Student(string n, int g) {
+//	name = n;
+//	grade = g;
+//}
+//
+//
+//Student::~Student()
+//{
+//}
+//
+//Student::print()
+//{
+//	cout << name << endl<< grade<< endl;
+//	
+//}
---
+++ main.cpp
@@ -0,0 +1,6 @@
+//#include "header.h"
+
+int main(void) {
+	/*Student first_student ();*/
+	return 0;
+}
